204e51f09a555aefaea8c48399579043cc1dbed5
YFP <yanina.flores@gmail.com>
2018-09-08 00:00:05 -0500

    Primer Commit Taller 1

---
+++ src/main/java/com/jbgroup/Tareajb1.java
@@ -0,0 +1,12 @@
+package com.jbgroup;
+
+public class Tareajb1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Mi Primer Tallercito - DevOp - GIT");
+
+	}
+
+}
